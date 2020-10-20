60d7463f06d9821ae418b9a688bccc73257c8b07
tmoran18 <moran.tim01@gmail.com>
2020-10-20 21:25:46 +1000

    Check about clearing saved planner before creating new planner

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Link from 'next/link';
 import { PlannerContext } from '../context/PlannerContext';
 import Layout from '../components/Layout';
 
 export default function Home() {
+	const router = useRouter();
 	// Planner Context
 	const { plannerSaved, setPlannerSaved } = useContext(PlannerContext);
 
@@ -13,6 +15,25 @@ export default function Home() {
 		localStorage.getItem('plannerSaved') !== null && setPlannerSaved(true);
 	}, []);
 
+	// Check if there is a saved planner, if there is throw a confirm you want to overwrite it alert
+	// If confirm, clear the planner and move onto new planner details
+	// Else go nowhere
+	const clearSavedPlanner = () => {
+		if (plannerSaved) {
+			let con = confirm(
+				'This will overwrite your saved planner, are you sure?',
+			);
+			// If confirm clear planner, and route to new planner details
+			con
+				? (setPlannerSaved(false),
+				  localStorage.clear(),
+				  router.push('/units-count'))
+				: //   Else route to current page
+				  router.push('/');
+		} else {
+			router.push('/units-count');
+		}
+	};
 	return (
 		<div>
 			<Head>
@@ -21,11 +42,8 @@ export default function Home() {
 			</Head>
 
 			<Layout>
-				<Link href='/units-count'>
-					<a>
-						<button>Create New Planner</button>
-					</a>
-				</Link>
+				<button onClick={clearSavedPlanner}>Create New Planner</button>
+
 				{plannerSaved && (
 					<Link href='/planner'>
 						<a>
